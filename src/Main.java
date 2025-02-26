c586b753dca1cf8375f7281c4b626c859a923eba
Pawel <platyn14@gmail.com>
2025-02-26 20:33:30 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 20;
+        Point p = new Point();
+        p.x = 10;
+        p.y = 20;
         // System.out.println(p1.toSvg());// p1.toString()
-        p1.translate(400,500);
-        System.out.println(p1);
-        Point p2=p1.translated(237,0);
+        p.translate(400,500);
+        System.out.println(p);
+        Point p2=p.translated(237,0);
         System.out.println(p2);
+        Segment s=new Segment();
+        s.p1.x=12;
+        s.p1.y=13;
+        s.p2.x=14;
+        s.p2.y=15;
+        System.out.println(s.length_o());
+        Segment []arr = new Segment[10];
+        double licznik=1.0;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=new Segment();
+           arr[i].p1.x=licznik;
+           arr[i].p1.y=licznik+1.0;
+           arr[i].p2.x=licznik+2.0;
+           arr[i].p2.y=licznik+3.0;
+           licznik+=4.0;
+        }
+        Segment maxi=arr[0].maximal(arr);
     }
 }
